d9d48ba40c6b6952a176e0caa874fb81744127bb
Stas Parshin <s.a.parshin@gmail.com>
2015-08-23 23:30:30 +0300

    Save dropboxApi in application class, auth in mainActivity

--- src/main/java/io/github/pengrad/uw_android_dropbox/MainActivity.java
+++ src/main/java/io/github/pengrad/uw_android_dropbox/MainActivity.java
@@ -3,11 +3,22 @@ package io.github.pengrad.uw_android_dropbox;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import com.dropbox.client2.DropboxAPI;
+import com.dropbox.client2.android.AndroidAuthSession;
+
+import butterknife.Bind;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
+
+    private MyApp app;
+
+    @Bind(R.id.linkDropbox) TextView mTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,15 +26,65 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         ButterKnife.bind(this);
+
+        app = MyApp.get(this);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        completeDropboxAuth();
+        updateDropboxStatus();
+    }
+
+    private void completeDropboxAuth() {
+        DropboxAPI<AndroidAuthSession> dropboxAPI = app.getDropboxApi();
+        if (dropboxAPI.getSession().authenticationSuccessful()) {
+            try {
+                // Required to complete auth, sets the access token on the session
+                dropboxAPI.getSession().finishAuthentication();
+                String accessToken = dropboxAPI.getSession().getOAuth2AccessToken();
+                app.saveDropboxAuth(accessToken);
+                Log.d("DbAuthLog", "Success " + accessToken);
+            } catch (IllegalStateException e) {
+                Log.d("++++", "Error authenticating", e);
+            }
+        }
+    }
+
+    private void updateDropboxStatus() {
+        if(isDropboxLinked()) {
+            mTextView.setText("Dropbox ist OK");
+            mTextView.setTextColor(getResources().getColor(R.color.green));
+        } else {
+            mTextView.setText("Link Dropbox Zuerst");
+            mTextView.setTextColor(getResources().getColor(R.color.blue));
+        }
+    }
+
+    @OnClick(R.id.linkDropbox)
+    void linkDropbox() {
+        if (!isDropboxLinked()) {
+            app.getDropboxApi().getSession().startOAuth2Authentication(this);
+        }
     }
 
     @OnClick(R.id.addImageSet)
     void openAddImagesActivity() {
-        startActivity(new Intent(this, AddImagesActivity.class));
+        if (isDropboxLinked()) {
+            startActivity(new Intent(this, AddImagesActivity.class));
+        } else {
+            Toast.makeText(this, "Link Dropbox Zuerst", Toast.LENGTH_SHORT).show();
+        }
     }
 
     @OnClick(R.id.buttonStatus)
     void openStatusActivity() {
 
+    }
+
+    boolean isDropboxLinked() {
+        return app.isDropboxLinked();
     }
 }
---
+++ src/main/java/io/github/pengrad/uw_android_dropbox/MyApp.java
@@ -0,0 +1,59 @@
+package io.github.pengrad.uw_android_dropbox;
+
+import android.app.Application;
+import android.content.Context;
+import android.preference.PreferenceManager;
+import android.text.TextUtils;
+
+import com.dropbox.client2.DropboxAPI;
+import com.dropbox.client2.android.AndroidAuthSession;
+import com.dropbox.client2.session.AppKeyPair;
+
+/**
+ * stas
+ * 8/23/15
+ */
+public class MyApp extends Application {
+
+    public static final String DROPBOX_TOKEN = "DROPBOX_TOKEN";
+
+    public static MyApp get(Context context) {
+        return (MyApp) context.getApplicationContext();
+    }
+
+    private DropboxAPI<AndroidAuthSession> mDBApi;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        AppKeyPair appKeys = new AppKeyPair(BuildConfig.API_KEY, BuildConfig.API_SECRET);
+        AndroidAuthSession session = new AndroidAuthSession(appKeys);
+
+        String accessToken = loadDropboxToken();
+        if (!TextUtils.isEmpty(accessToken)) {
+            session.setOAuth2AccessToken(accessToken);
+        }
+
+        mDBApi = new DropboxAPI<>(session);
+    }
+
+    public DropboxAPI<AndroidAuthSession> getDropboxApi() {
+        return mDBApi;
+    }
+
+    public boolean isDropboxLinked() {
+        return mDBApi.getSession().isLinked();
+    }
+
+    private String loadDropboxToken() {
+        return PreferenceManager.getDefaultSharedPreferences(this).getString(DROPBOX_TOKEN, null);
+    }
+
+    public void saveDropboxAuth(String accessToken) {
+        PreferenceManager.getDefaultSharedPreferences(this)
+                .edit()
+                .putString(DROPBOX_TOKEN, accessToken)
+                .apply();
+    }
+}
